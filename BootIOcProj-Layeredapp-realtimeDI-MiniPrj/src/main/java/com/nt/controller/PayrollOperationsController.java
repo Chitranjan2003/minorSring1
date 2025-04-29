c17d1082e077926152c26d01717d7570b734b69e
Acer <Acer@DESKTOP-4UKMIK6.Dlink>
2025-04-29 22:09:50 +0530

    payroll

--- BootIOcProj-Layeredapp-realtimeDI-MiniPrj/src/main/java/com/nt/controller/PayrollOperationsController.java
+++ BootIOcProj-Layeredapp-realtimeDI-MiniPrj/src/main/java/com/nt/controller/PayrollOperationsController.java
@@ -15,7 +15,7 @@ public class PayrollOperationsController {
 	private IEmployeeService service;
 	
 	public List<Employee> showEmployeesByDesgs(String desg1,String desg2,String desg3)throws Exception{
-		
+		System.out.println("PayrollOperationsController.showEmployeesByDesgs()");
 		List<Employee> list =service.fetchAAllEmployeesDesgs(desg1, desg2, desg3);
 		return list;
 	}
